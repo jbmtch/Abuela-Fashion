c06e5ae5842443b24fd7f89f2e3fc9ff1bf07707
Justin Mitchell <jmitchell31@g.ucla.edu>
2022-01-13 23:39:18 -0800

    Successfully formed query string to return review with photos property and array of photo objects inside of it

--- server/databases/database.js
+++ server/databases/database.js
@@ -1,11 +1,32 @@
 
 var pgp = require('pg-promise')();
 const { options } = require('./dbconfig.js');
-
 const db = pgp(options);
 
-console.log('it worked');
+var getAllReviews = function(callback, productId = 2, page = 1, count = 5) {
+  var queryString = 'SELECT reviews.*, json_agg(photos.*) AS photos FROM public."photos" LEFT JOIN public."reviews" ON reviews.id = photos.review_id GROUP BY reviews.id limit $2'
+  let total = page * count;
+  // var queryString = 'SELECT * FROM reviews WHERE product_id = $1 limit $2';
+  var params = [productId, total]
+  db.query(queryString , params)
+  .then((results) => {
+    console.log(results);
+    callback(null, results);
+  })
+  .catch((error) => {
+    callback(error, null);
+  })
+}
 
+
+
+console.log('Successfully connected to postgres db: ', options.database);
+
+
+module.exports = {
+  getReviews: getAllReviews,
+
+}
 
 
 
--- server/databases/server.js
+++ server/databases/server.js
@@ -1,9 +1,23 @@
 const express = require('express');
+//const cors = require('cors')
 const app = express();
 const port = 5555;
 const db = require('./database.js');
 
+// app.use(express.static('../client/dist'));
+app.use(express.json());
 
+
+app.get('/home', function(req, res) {
+  db.getReviews((err, results) => {
+    if (err) {
+      console.error(err);
+    } else {
+      res.send(results);
+    }
+  });
+
+})
 
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
