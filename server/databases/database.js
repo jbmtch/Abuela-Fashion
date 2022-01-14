6147cc8058dea2b10269f36db90e3b5da47e4dcc
Justin Mitchell <jmitchell31@g.ucla.edu>
2022-01-14 15:35:05 -0800

    successfully returning proper object for getting all reviews for a particular product

--- server/databases/database.js
+++ server/databases/database.js
@@ -3,16 +3,23 @@ var pgp = require('pg-promise')();
 const { options } = require('./dbconfig.js');
 const db = pgp(options);
 
-var getAllReviews = function(callback, productId = 1, page = 2, count = 5) {
+var getAllReviews = function(callback, productId = 5, page = 2, count = 5) {
   // var queryString = 'SELECT reviews.*, json_agg(photos.*) AS photos FROM public."reviews" LEFT JOIN public."photos" ON reviews.id = photos.review_id GROUP BY reviews.id limit $2';
   var queryString = 'SELECT reviews.*, COALESCE(json_agg(photos.*) FILTER (WHERE photos.review_id IS NOT NULL), \'[]\') AS photos FROM public."reviews" LEFT JOIN public."photos" ON reviews.id = photos.review_id where product_id = $1 GROUP BY reviews.id limit $2';
-
   let total = page * count;
   var params = [productId, total]
   db.query(queryString , params)
   .then((results) => {
-    console.log(results);
-    callback(null, results);
+    results.forEach((review) => {
+      var date = new Date(Number(review.date));
+      review['date'] = date;
+    })
+    var obj = {};
+    obj['product'] = productId;
+    obj['page'] = page;
+    obj['count'] = count;
+    obj['results'] = results;
+    callback(null, obj);
   })
   .catch((error) => {
     callback(error, null);
