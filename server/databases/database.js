49b9116c83c874f007f4a7191f8bd2f085661754
Justin Mitchell <jmitchell31@g.ucla.edu>
2022-01-14 11:08:49 -0800

    Realized that query string was wrong which was why it was running so slow. Fixed it

--- server/databases/database.js
+++ server/databases/database.js
@@ -4,9 +4,8 @@ const { options } = require('./dbconfig.js');
 const db = pgp(options);
 
 var getAllReviews = function(callback, productId = 2, page = 1, count = 5) {
-  var queryString = 'SELECT reviews.*, json_agg(photos.*) AS photos FROM public."photos" LEFT JOIN public."reviews" ON reviews.id = photos.review_id GROUP BY reviews.id limit $2'
+  var queryString = 'SELECT reviews.*, json_agg(photos.*) AS photos FROM public."reviews" LEFT JOIN public."photos" ON reviews.id = photos.review_id GROUP BY reviews.id limit $2'
   let total = page * count;
-  // var queryString = 'SELECT * FROM reviews WHERE product_id = $1 limit $2';
   var params = [productId, total]
   db.query(queryString , params)
   .then((results) => {
